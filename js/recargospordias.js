b75de108f6f79ee51b34d19eb5e0ece90a9b16c8
Carlos Danilo Angarita Garcia <82780561+carlos060798@users.noreply.github.com>
2022-12-22 23:22:28 -0500

    comir de actulizacion dos

--- js/recargospordias.js
+++ js/recargospordias.js
@@ -1,30 +1,35 @@
-const inputsalario = document.querySelector('#salario');
-const inputinicio= document.querySelector('#inicio');
-const inputdefin= document.querySelector('#fin');
-const inpuTSUB= document.querySelector('#subsidio');
-const btn = document.querySelector('#calcular');
-const pcesantis= document.querySelector('#cesantias');
-const pintereses = document.querySelector('#interes');
-const pprima = document.querySelector('#prima');
-const pvacaciones=document.querySelector('#vacaciones');
-const ptotal=document.querySelector('#total')
-const pdias=document.querySelector('#dias')
+const inputsalariod = document.querySelector('#salario');
+const inputiniciod= document.querySelector('#inicio');
+const inputdefind= document.querySelector('#fin');
+const inpuTSUBd= document.querySelector('#subsidio');
+const btnd = document.querySelector('#Dias');
+const pcesantisd= document.querySelector('#cesantias');
+const pinteresesd= document.querySelector('#interes');
+const pprimad= document.querySelector('#prima');
+const pprima2d= document.querySelector('#prima2')
+const pvacacionesd=document.querySelector('#vacaciones');
+const paportesd= document.querySelector('#aportesSP')
+const ptotald=document.querySelector('#total')
+const pdiasd=document.querySelector('#dias') 
+const pmensajed=document.querySelector('#mensaje') 
 
 
 
 
 
 
-btn.addEventListener('click', calcularPrecioConDescuento);
 
 
-function calcularPrecioConDescuento(event){
+btnd.addEventListener('click', recargosdias);
+
+
+function recargosdias(event){
   event.preventDefault();
 //variables 
-let salario= Number(inputsalario.value);
-let subcidio=Number(inpuTSUB.value);
-let inicio = new Date(inputinicio.value);
-let  fin = new Date(inputdefin.value);
+let salario= Number(inputsalariod.value);
+let subcidio=Number(inpuTSUBd.value);
+let inicio = new Date(inputiniciod.value);
+let  fin = new Date(inputdefind.value);
 
 console.log({
   salario,subcidio,inicio,fin
@@ -43,30 +48,44 @@ console.log({
   }
   
   let  dias= calculardias(inicio,fin) 
-  pdias.innerText=dias
+  if(dias>7){
+   alert('dias no pueden ser mayores a 6')
+  } else if(dias<7){
+ 
+  pdiasd.innerText=dias
   
   console.log(dias)
   
   // ciclo validador de llenado de spacios
   let salariofinal=salario+subcidio
   const cesantias= Math.round((salariofinal*dias) / 360);
-  pcesantis.innerText = cesantias;
+  pcesantisd.innerText = cesantias;
+  let prima1
+  let prima2
+  if (dias <= 6) {
+    prima1 = dias
+    prima2 = 0
+    console.log(prima1, prima2)
+  } 
  
  const intereses = Math.round((cesantias*0.12*dias) /360);
- pintereses.innerText = intereses; 
+ pinteresesd.innerText = intereses; 
 
 
  const prima = Math.round((salariofinal*dias) / 360);
- pprima.innerText= prima;
-
+ pprimad.innerText= prima;
+ const primasegundo = Math.round((salariofinal * prima2) / 360);
+ pprima2d.innerText = primasegundo.toLocaleString();
 
  const vacaciones= Math.round((salariofinal*dias) / 720);
- pvacaciones.innerText = vacaciones; 
+ pvacacionesd.innerText = vacaciones; 
+ let aportestrabajador= (salario*8) / 100;
+ paportes.innerText = aportestrabajador.toLocaleString();
   
  const totalprestaciones= cesantias+vacaciones+intereses+prima;
- ptotal.innerText= totalprestaciones;  
+ ptotald.innerText= totalprestaciones;  
       
+  }
  }
-
 
 }
